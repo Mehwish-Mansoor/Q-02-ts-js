3d0a9105f3e63961b35210e6a6bd4bb703c4d1e3
Mehwish-Mansoor <160300292+Mehwish-Mansoor@users.noreply.github.com>
2024-03-08 09:26:14 +0500

    Add files via upload

---
+++ Qno2.js
@@ -0,0 +1,5 @@
+"use strict";
+// Persona Message: Store a person's name in a variable, and print a person to that person . your message should be simple, such as," Hello Eric,
+// would you like to learn some python?""
+let firstName = "Eric";
+console.log(`"Hello ${firstName}would you like to learn some pyhton today"`);
---
+++ Qno2.ts
@@ -0,0 +1,6 @@
+// Persona Message: Store a person's name in a variable, and print a person to that person . your message should be simple, such as," Hello Eric,
+// would you like to learn some python?""
+
+
+let firstName: string= "Eric"
+console.log(`"Hello ${firstName}would you like to learn some pyhton today"`)   
